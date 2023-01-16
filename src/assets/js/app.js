6becec6a17706fa1156785e2674ef1eba6c02a03
Nataliia <kurinak@inbox.ru>
2023-01-16 15:06:25 +0400

    add adaptive version blocks and add catalog-menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,4 +12,22 @@ document.addEventListener('DOMContentLoaded', function () {
             prevEl: '.site-nav__btn--prev'
         },
     });
+
+
+    // Menu Toggle
+    // ****************
+    const burgerToggle = document.getElementById('burger');
+    const burgerClose = document.getElementById('burger-close');
+    const catalogMenuShow = document.getElementById('catalog-menu');
+    const bodyDisableScroll = document.querySelector('body');
+
+    burgerToggle.addEventListener('click', () => {
+        catalogMenuShow.classList.add('catalog-menu--show');
+        bodyDisableScroll.classList.add('no-scroll');
+    });
+
+    burgerClose.addEventListener('click', () => {
+        catalogMenuShow.classList.remove('catalog-menu--show');
+        bodyDisableScroll.classList.remove('no-scroll');
+    });
 });
